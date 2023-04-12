1c67f48836e093c61c8ea3533863464691c3fb47
My Phung Tieu <tmp03099@gmail.com>
2023-04-12 09:38:24 -0400

    server is running

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+//! 1
 const express = require("express");
 
 //create app
 const app = express();
 const PORT = 3000;
 
+//! 3
+// default route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Pokemon App!");
+});
+
+//! 2
 // set listen for port
 app.listen(PORT, function () {
   console.log(`Server runing on port ${PORT}`);
